abb225cc73f1ffbf0a2afb55d9318a3b0e462e01
gunduSama <mayurnangare10@gmail.com>
2021-07-28 11:31:56 +0530

    Unity RPG Protorype Ver 0.1
    Basic RPG Game Play ;
    Current Features -> camera follow,click to move character, character animations useing Unity Blend Tree

---
+++ RPG prototype 1/Assets/Scripts/Core/FollowCamera.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 0.5f;
+
+   
+    private void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+} //class
---
+++ RPG prototype 1/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    private void Update()
+   {
+       if (Input.GetMouseButton(0))
+       {
+           MoveToCursor();
+       }
+
+       UpdateAnimator();
+   }
+   
+    public void MoveToCursor()
+   {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // shoots a ray from the main camera origin;
+        RaycastHit hit; // gets the hit point;
+        bool hasHit = Physics.Raycast(ray, out hit); // returns a bool if the ray hits a collider;
+
+       if (hasHit)
+       {
+           GetComponent<NavMeshAgent>().destination = hit.point; // if true then sets the players position to mouse position;
+       }
+   } // method
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed",speed);
+    }
+    
+} // class
